0f483c0c0afb32183252ade646fd7c0300e6e643
aviab <Mar2015!>
2016-11-14 17:54:46 +0200

    first commit

---
+++ ProfileParsing.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProfileParsing.Domain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("888 Holdings")]
+[assembly: AssemblyProduct("ProfileParsing.Domain")]
+[assembly: AssemblyCopyright("Copyright © 888 Holdings 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b08004ef-1877-437d-9612-90f885f06724")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProfileParsing/Controllers/ProfileController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace ProfileParsing.Controllers
+{
+    public class ProfileController : ApiController
+    {
+        [Route("api/profile")]
+        [HttpPost]
+        public async Task<IHttpActionResult> setProfile(string profileUrl)
+        {
+
+            try
+            {
+                
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        [Route("api/searchPeople")]
+        [HttpPost]
+        public async Task<IHttpActionResult> searchPeople()
+        {
+
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        [Route("api/peopleBySkills")]
+        [HttpPost]
+        public async Task<IHttpActionResult> peopleBySkills()
+        {
+
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
